11a54a41dff2ddd4694f70037e3cb3b9e0f15f68
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-19 12:08:09 +0100

    Is-number package import

--- src/index.js
+++ src/index.js
@@ -1,31 +1,5 @@
-import {inc, dec, getCounter} from './counter'
+import * as isNumber from 'is-number'
 
-const incBtn = document.createElement('button')
-const decBtn = document.createElement('button')
-
-incBtn.innerHTML = 'PLUS'
-decBtn.innerHTML = 'MINUS'
-
-incBtn.addEventListener(
-    'click',
-    () => {
-        inc()
-        console.log(getCounter())
-    }
-)
-
-decBtn.addEventListener(
-    'click',
-    () => {
-        dec()
-        console.log(getCounter())
-    }
-)
-
-document.body.appendChild(incBtn)
-document.body.appendChild(decBtn)
-
-// we want to import counter from module named counter
-// and attach functions from module to buttons
-// and display in console actual value of counter
-// when it changes
+console.log(isNumber(6))
+console.log(isNumber('6'))
+console.log(isNumber('06'))
